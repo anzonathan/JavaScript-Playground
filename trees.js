52fa6d6a482e1d82f7fa8673f3a53cd13b513a8b
Nathaniel Anzo Mugenyi <41405954+anzonathan@users.noreply.github.com>
2024-12-20 12:36:14 +0300

    Create trees.js

---
+++ trees.js
@@ -0,0 +1,13 @@
+//Tress
+
+const prompt = require("prompt-sync")();
+
+
+var no = prompt("How tall is my tree? "); //height of tree
+var leaf = "#";//counter for leaves
+
+for (let i = 0; i < no; i++){
+
+    console.log(leaf);
+    leaf += "#"
+}
